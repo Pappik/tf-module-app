38da4d39c6599a010b08f0ad1ed82648b141e979
pappik <pappik@gmail.com>
2024-01-04 23:22:19 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -1,20 +1,12 @@
-resource "aws_docdb_subnet_group" "default" {
-  name       = "${var.env}-docdb_subnet_group"
-  subnet_ids = var.subnet_ids
-
-  tags = merge(local.common_tags, { Name = "${var.env}-docdb_subnet_group"} )
-
-}
-
-resource "aws_security_group" "docdb" {
-  name        = "${var.env}-docdb_security_group"
-  description = "${var.env}-docdb_security_group"
+resource "aws_security_group" "main" {
+  name        = "${var.env}-${var.component}-security-group"
+  description = "${var.env}-${var.component}-security-group"
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "Mongodb"
-    from_port   = 27017
-    to_port     = 27017
+    description = "HTTP"
+    from_port   = var.app_port
+    to_port     = var.app_port
     protocol    = "tcp"
     cidr_blocks = var.allow_cidr
   }
@@ -26,26 +18,5 @@ resource "aws_security_group" "docdb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = merge(local.common_tags, { Name = "${var.env}-docdb_security_group"} )
-}
-
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "${var.env}-docdb-cluster"
-  engine_version          = var.engine_version
-  master_password         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
-  master_username         = data.aws_ssm_parameter.DB_ADMIN_USER.value
-  skip_final_snapshot     = true
-  db_subnet_group_name = aws_docdb_subnet_group.default.name
-  vpc_security_group_ids = [aws_security_group.docdb.id]
-
-  tags = merge(local.common_tags, { Name = "${var.env}-docdb_security_group"} )
-}
-
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = var.number_of_instances
-  identifier         = "${var.env}-docdb-cluster-instances-${count.index + 1 }"
-  cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = "db.r5.large"
-
-  tags = merge(local.common_tags, { Name = "${var.env}-docdb-cluster-instances-${count.index + 1 }"} )
+  tags = merge(local.common_tags, { Name = "${var.env}-${var.component}-security-group"} )
 }
