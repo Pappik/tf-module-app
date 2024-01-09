204f632520d7cd0a516e1696625d5baf99e82ed2
pappik <pappik@gmail.com>
2024-01-09 13:37:55 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(local.common_tags, { Name = "${var.env}-${var.component}-role"} )
+
+}
+
 resource "aws_security_group" "main" {
   name        = "${var.env}-${var.component}-security-group"
   description = "${var.env}-${var.component}-security-group"
@@ -11,6 +32,14 @@ resource "aws_security_group" "main" {
     cidr_blocks = var.allow_cidr
   }
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.bastion_cidr
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -22,33 +51,34 @@ resource "aws_security_group" "main" {
 }
 
 #
-#resource "aws_launch_template" "main" {
-#  name_prefix   = "${var.env}-${var.component}-template"
-#  image_id      = data.aws_ami.centos8.id
-#  instance_type = var.instance_type
-#}
-#
-#
-#resource "aws_autoscaling_group" "asg" {
-#  name                      = "${var.env}-${var.component}-asg"
-#  max_size                  = var.max_size
-#  min_size                  = var.min_size
-#  desired_capacity          = var.desired_capacity
-#  force_delete              = true
-#  vpc_zone_identifier       = var.subnet_ids
-#
-#  launch_template {
-#    id    = aws_launch_template.main.id
-#    version = "$Latest"
-#  }
-#
-#  dynamic "tag" {
-#    for_each = local.all_tags
-#    content {
-#      key = tag.value.key
-#      value = tag.value.value
-#      propagate_at_launch = true
-#    }
-#  }
-#
-#}
+resource "aws_launch_template" "main" {
+  name_prefix   = "${var.env}-${var.component}-template"
+  image_id      = data.aws_ami.centos8.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+}
+
+
+resource "aws_autoscaling_group" "asg" {
+  name                      = "${var.env}-${var.component}-asg"
+  max_size                  = var.max_size
+  min_size                  = var.min_size
+  desired_capacity          = var.desired_capacity
+  force_delete              = true
+  vpc_zone_identifier       = var.subnet_ids
+
+  launch_template {
+    id    = aws_launch_template.main.id
+    version = "$Latest"
+  }
+
+  dynamic "tag" {
+    for_each = local.all_tags
+    content {
+      key = tag.value.key
+      value = tag.value.value
+      propagate_at_launch = true
+    }
+  }
+
+}
