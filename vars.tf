f041a852c834f17a040a6e81dc7c0aa1436e1596
pappik <pappik@gmail.com>
2024-01-09 11:49:56 -0500

    vpc terraform

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
-variable "env" {}
-variable "subnet_ids" {}
-variable "vpc_id" {}
-variable "allow_cidr" {}
-variable "component" {}
-variable "app_port" {}
+#variable "env" {}
+#variable "subnet_ids" {}
+#variable "vpc_id" {}
+#variable "allow_cidr" {}
+#variable "component" {}
+#variable "app_port" {}
