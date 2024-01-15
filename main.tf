63c804e0bbf18762892742a69d925861123c8d53
pappik <pappik@gmail.com>
2024-01-15 12:51:11 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -187,9 +187,9 @@ resource "aws_lb_listener" "frontend" {
   port              = "80"
   protocol          = "HTTP"
 
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
 }
 
-default_action {
-  type             = "forward"
-  target_group_arn = aws_lb_target_group.target_group.arn
-}
